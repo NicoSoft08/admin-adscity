c6fc3a7c7b1c71aad23a004e929a7f94f9064f8d
Nicolas Peniel <135339436+NicoSoft08@users.noreply.github.com>
2025-05-14 18:13:47 +0300

    Add files via upload

---
+++ src/layouts/AdminLayout.jsx
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect, useState } from 'react'
+import Sidebar from '../components/navigation/Sidebar';
+import Header from '../components/Header';
+import { Outlet } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
+
+const homeURL = process.env.REACT_APP_HOME_URL;
+const authURL = process.env.REACT_APP_AUTH_URL;
+
+export default function AdminLayout() {
+    const { currentUser, userRole, userData } = useContext(AuthContext);
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+
+    console.log(currentUser, userData);
+    useEffect(() => {
+        if (!currentUser) {
+            window.location.href = `${authURL}/signin`;
+        } else if (userRole !== 'admin') {
+            window.location.href = `${homeURL}/access-denied`;
+        }
+    }, [currentUser, userRole]);
+
+    const toggleSidebar = () => {
+        setSidebarOpen(!sidebarOpen);
+    };
+
+    return (
+        <div className="account-layout">
+            <Sidebar isOpen={sidebarOpen} closeSidebar={() => setSidebarOpen(false)} />
+
+            <div className="layout-content">
+                <Header toggleSidebar={toggleSidebar} />
+                <main className="main-content">
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    )
+}
